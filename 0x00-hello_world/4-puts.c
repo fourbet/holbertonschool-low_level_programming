8e6b22be76e5a0fe719a64b2e9e1290222a321e1
fourbet <ofourbet@yahoo.fr>
2020-09-16 16:45:02 +0000

    fixed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main() {
-puts("\"Programming is like building a multilingual puzzle");
-return 0;
-}
+/**
+ *main - puts
+ *Return: 0
+ */
+int main(void)
+{puts("\"Programming is like building a multilingual puzzle");
+return (0); }
