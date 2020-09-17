5c0162870235af5234491e0d36a044fa3a29988c
fourbet <ofourbet@yahoo.fr>
2020-09-17 09:33:17 +0000

    first try

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main -numbers
+ *
+ * Result: 0
+ */
+int main(void)
+{
+  int num;
+  for(num = 1 ; num < 10 ; num++)
+    { printf("%d", num);
+    }
+  putchar('\n');
+ return (0);
+}
