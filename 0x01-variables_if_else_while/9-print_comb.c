45a5c384cd67f3c020bf902dfc5efd7ae5ba2e3a
fourbet <ofourbet@yahoo.fr>
2020-09-17 14:28:54 +0000

    fixed

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,16 +10,16 @@
  */
 int main(void)
 {
-  int i;
-  i = 0;
+int i;
+i = 0;
 while (i < 10)
-  {putchar((i % 10) + '0');
-    if ( i != 9)
-      {
-    putchar(',');
-    putchar(' ');
-      }
-    i++;
+{putchar((i % 10) + '0');
+if (i != 9)
+{
+putchar(',');
+putchar(' ');
+}
+i++;
 }
 putchar('\n');
 return (0);
