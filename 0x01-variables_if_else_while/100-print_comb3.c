a3bd760e4ec667106bd24a461d7c0da412101c15
fourbet <ofourbet@yahoo.fr>
2020-09-18 06:11:43 +0000

    combination 3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - combination 3
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+char a;
+char b;
+i = 0;
+while (i < 100)
+{a = ((i / 10) + '0'); /*first digit*/
+b = ((i % 10) + '0'); /* last digit*/
+if (a != b && a < b)
+{
+if (i != 1)
+{
+putchar(',');
+putchar(' ');
+}
+putchar(a);
+putchar(b);
+}
+i++;
+}
+putchar('\n');
+return (0);
+}
