702facf5e1a0deea5b96f643b5ec9c5286fb674a
fourbet <ofourbet@yahoo.fr>
2020-09-21 15:45:34 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet - prints alphabet in lowercase
+ *
+ * Return: void (Success)
+ */
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
