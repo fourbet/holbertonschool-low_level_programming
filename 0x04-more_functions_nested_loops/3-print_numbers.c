d7e0ef10455b5a4f6fe6dba6184bfbf5f1484f15
fourbet <ofourbet@yahoo.fr>
2020-09-24 15:35:09 +0000

    first try

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - multiples two interger
+ * 
+ * Return: result (Success)
+ */
+
+void print_numbers(void)
+{
+	int i;
+	
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+
+}
