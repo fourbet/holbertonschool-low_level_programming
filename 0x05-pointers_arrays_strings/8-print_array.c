261c778a8462ef3fd969fa1abd8b6e01675554ef
fourbet <ofourbet@yahoo.fr>
2020-09-28 18:20:21 +0000

    print array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elemenrs of an array of intergers
+ * @a: pointer to an int
+ * @n: int
+ *
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		if (i != (n-1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
