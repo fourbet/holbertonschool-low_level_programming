a7b6a8a9833749617f9e47afa5dd94b7629d5e81
fourbet <ofourbet@yahoo.fr>
2020-09-28 16:04:00 +0000

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_rev - prints a string in reverse
+ * @s: pointer to a char
+ *
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	i--; /* remove 1 */
+
+	while (i >= 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
