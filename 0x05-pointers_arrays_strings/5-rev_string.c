2ad851a2bda904a034bef2f74a929ff33d08f78d
fourbet <ofourbet@yahoo.fr>
2020-09-29 12:46:56 +0000

    fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 #include <stdio.h>
 /**
- * rev-string - reverse string
+ * rev_string - reverse string
  * @s: pointer to a char
  *
  * Return: void
@@ -12,24 +12,19 @@ void rev_string(char *s)
 	int i = 0;
 	char tmp = '0';
 	int j = 0;
-	
+
 	while (s[i] != '\0')
 	{
 		i++;
 	}
 	i--; /* length s */
 	j = 0;
-	while( j <= i)
+	while (j <= i)
 	{
-		tmp= s[j];
+		tmp = s[j];
 		s[j] = s[i];
-		printf(" Value of tmp, s[j] ,j ,i : %c, %d, %d, %d \n",tmp, s[j], j, i);
-		*(s+i) = tmp;
+		*(s + i) = tmp;
 		j++;
 		i--;
 	}
-	printf(" Value of i,j : %d, %d \n", i,j);
-	printf(" Value of c[8] : %d\n", s[8]);
-	printf("Value of c[0] : %d\n", s[0]);
-	
 }
