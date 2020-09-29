a933d4c541864c306eafa7d0ee39641223368185
fourbet <ofourbet@yahoo.fr>
2020-09-29 07:17:49 +0000

    fixed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,14 +11,12 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i = 0;
-	int len = 0;
 
-	len = _strlen(src);
 	while (src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	dest[len] = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
