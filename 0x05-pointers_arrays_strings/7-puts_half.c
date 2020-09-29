75511f0cf62999e6e5170fc0b73d418ce8af52a6
fourbet <ofourbet@yahoo.fr>
2020-09-29 06:59:34 +0000

    fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,5 +1,5 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * puts_half - prints half
  * @str: pointer to a char
@@ -11,22 +11,29 @@ void puts_half(char *str)
 {
 	int i = 0;
 	int len = 0;
-	int half;
+	int half = 0;
+	int half1 = 0;
+	int j = 0;
 
-	len = _strlen(str);
-
+	while (str[j] != '\0')
+	{
+		len++;
+		j++;
+	}
 	if (len % 2 == 0)
 	{
 		half = len / 2;
 	}
 	else
-		half = (len - 1) / 2;
-
+	{	half1 = (len - 1) / 2;
+		half = len - half1;
+	}
 	while (str[i] != '\0')
 	{
-		if (i > half)
-			_putchar(str[i]);
-		i++;
+		if (i >= half)
+		{	_putchar(str[i]);
+		}
+	i++;
 	}
 
 	_putchar('\n');
