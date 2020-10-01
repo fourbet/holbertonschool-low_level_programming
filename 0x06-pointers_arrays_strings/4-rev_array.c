ebd5cd17a138827c1bfc6851824927166c125798
fourbet <ofourbet@yahoo.fr>
2020-10-01 13:52:20 +0000

    add exo 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * reverse_array - reverses the content of an array of integers.
+ * @a: pointer to an int
+ * @n: number of elements of the array
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	
+	int i = 0;
+	int tmp = 0;
+
+	while (i < n)
+	{
+		tmp = a[i];
+		a[i] = a[n - 1];
+		a[n - 1] = tmp;	
+		i++;
+		n--;
+	}
+}
