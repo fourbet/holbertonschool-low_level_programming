2aa2c9d2d64187a02c6e0dd94b923f0d79573bae
fourbet <ofourbet@yahoo.fr>
2020-10-02 08:31:58 +0000

    fixed 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -22,12 +22,6 @@ char *cap_string(char *s)
 				s[i + 1] = s[i + 1] - ' '; /* - 32 */
 			}
 		}
-		else if (s[i] == '\t')
-		{
-			s[i] = ' ';
-			if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				s[i + 1] = s[i + 1] - ' ';
-		}
 		i++;
 	}
 	return (s);
