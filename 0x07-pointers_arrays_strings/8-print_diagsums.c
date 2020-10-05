cda986613327eea6e637cb2a127737db69dc910d
fourbet <ofourbet@yahoo.fr>
2020-10-05 19:32:17 +0000

    fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 /**
  * print_diagsums - prints the sum of the two diagonals
@@ -11,15 +11,15 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
-	long int sum1 = 0;
-	long int sum2 = 0;
+	int sum1 = 0;
+	int sum2 = 0;
 
 	for (i = 0; i < size * size; i++)
 	{
 		if (i % (size + 1) == 0)
-			sum1 += a[i];
+			sum1 += a[i] * 1;
 		if (i % (size - 1) == 0 && i != (size * size) - 1)
-			sum2 += a[i];
+			sum2 += a[i] * 1;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
