e3bab5c6436220f1cecc2c0e60930e3d0732b139
fourbet <ofourbet@yahoo.fr>
2020-10-05 08:26:09 +0000

    add exercise 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: pointer to a char
+ * @c: character
+ *
+ * Return: pointer to the first occurences of character c
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *res;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			res = &s[i];
+			break;
+		}
+		i++;
+
+	}
+	return (res);
+}
