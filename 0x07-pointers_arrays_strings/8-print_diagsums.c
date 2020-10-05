557a9be01ce1d4899cdf8694704d0fa407e293f1
fourbet <ofourbet@yahoo.fr>
2020-10-05 19:32:49 +0000

    fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -17,9 +17,9 @@ void print_diagsums(int *a, int size)
 	for (i = 0; i < size * size; i++)
 	{
 		if (i % (size + 1) == 0)
-			sum1 += a[i] * 1;
+			sum1 += a[i];
 		if (i % (size - 1) == 0 && i != (size * size) - 1)
-			sum2 += a[i] * 1;
+			sum2 += a[i];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
