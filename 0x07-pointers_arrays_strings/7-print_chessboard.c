cf026a976a14f9a11566fc8a41315b348b0580f5
fourbet <ofourbet@yahoo.fr>
2020-10-05 13:06:44 +0000

    add exercise 7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @s: pointer to a char
+ * @a
+ *
+ * Return: pointer to the first occurences of character c
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int j = 0;
+	
+	for ( i = 0; i < 8; i++)
+	{
+		for ( j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
