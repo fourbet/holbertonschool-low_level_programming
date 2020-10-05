2ce43a38c0bce552a05dabeb4f01e1c605082452
fourbet <ofourbet@yahoo.fr>
2020-10-05 19:31:00 +0000

    tried long int

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -11,8 +11,8 @@
 void print_diagsums(int *a, int size)
 {
 	int i = 0;
-	unsigned int sum1 = 0;
-	unsigned int sum2 = 0;
+	long int sum1 = 0;
+	long int sum2 = 0;
 
 	for (i = 0; i < size * size; i++)
 	{
