123e022ef7a5222b56ee53de11819361607900bc
fourbet <ofourbet@yahoo.fr>
2020-10-13 10:08:15 +0000

    add exercise 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,66 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - length of string
+ *
+ *@str: pointer to a string
+ *
+ * Return: int, length
+ */
+int _strlen(char *str)
+{
+	int i =0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * str_concat - concatenates two strings
+ *
+ * @s1: pointer to a string
+ * @s2: pointer to a string
+ *
+ * Return: pointer to a char
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char* s;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int len1 = 0;
+	int len2 = 0;
+	printf("s1 :%d\n", _strlen(s1));
+	printf("s2 :%d\n", _strlen(s2));
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	if (s1 == NULL)
+	{
+		s1 = "";
+		len1 = 0;
+	}
+	else
+		len1 = _strlen(s1);
+	if (s2 == NULL)
+        {
+		s2 ="";
+		len2 = 0;
+	}
+	else
+		len2 = _strlen(s2);
+	s = malloc(sizeof(char) * (len1 + len2));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len1 ; i++)
+		*(s + i) = *(s1 + i);
+	printf("i :%d\n",i);
+	for (j = i; j < i + len2 ; j++)                                                                      {
+		*(s + j) = *(s2 + k);
+		k++;
+	}
+	*(s + j) = '\0';
+	return (s);
+}
