34a550f3d24dfb0599d49df8db60726817f63e4b
fourbet <ofourbet@yahoo.fr>
2020-10-13 12:05:32 +0000

    fixed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+BB#include "holberton.h"
 #include <stdlib.h>
 #include <stdio.h>
 /**
@@ -10,7 +10,7 @@
  */
 int _strlen(char *str)
 {
-	int i =0;
+	int i = 0;
 
 	while (str[i] != '\0')
 		i++;
@@ -27,37 +27,40 @@ int _strlen(char *str)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char* s;
+	char *s;
 	int i = 0;
 	int j = 0;
 	int k = 0;
 	int len1 = 0;
 	int len2 = 0;
+
 	printf("s1 :%d\n", _strlen(s1));
 	printf("s2 :%d\n", _strlen(s2));
-	len1 = _strlen(s1);
-	len2 = _strlen(s2);
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 	if (s1 == NULL)
 	{
-		s1 = "";
 		len1 = 0;
+		s = malloc(sizeof(char) * (_strlen(s2) + 1));
+		s = *s2;
+		return (s);
 	}
 	else
-		len1 = _strlen(s1);
+		len1 = _strlen(s1) + 1;
 	if (s2 == NULL)
-        {
-		s2 ="";
+	{
 		len2 = 0;
 	}
 	else
-		len2 = _strlen(s2);
+		len2 = _strlen(s2) + 1;
 	s = malloc(sizeof(char) * (len1 + len2));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0; i < len1 ; i++)
+	for (i = 0; i < len1 - 1; i++)
 		*(s + i) = *(s1 + i);
-	printf("i :%d\n",i);
-	for (j = i; j < i + len2 ; j++)                                                                      {
+	printf("i :%d\n", i);
+	for (j = i; j < i + len2 - 1 ; j++)
+	{
 		*(s + j) = *(s2 + k);
 		k++;
 	}
