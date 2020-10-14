5d1c2c3e8c2211b0d7fa313ff1f09168d5e2b99b
fourbet <ofourbet@yahoo.fr>
2020-10-14 06:25:32 +0000

    add exercise 5

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,59 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _strlen - length of string
+ *
+ *@str: pointer to a string
+ *
+ * Return: int, length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * argstostr - concatenates all the arguments of the program
+ *
+ * @ac: int
+ * @av: pointer to a pointer to a char
+ *
+ * Return: pointer to a char
+ */
+char *argstostr(int ac, char **av)
+{
+	char *s = '\0';
+	int len = -6;
+	int stockAc = ac;
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	printf("ac : %d\n", ac);
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (ac--)
+	{
+		len += _strlen(av[ac]);
+	}
+	printf("total length :%d\n", len);
+	printf(" stockAc : %d\n", ac);
+	s = malloc(sizeof(char) * (len + stockAc));
+	if (s == NULL)
+		return (NULL);	
+	for (i =0; i < stockAc; i++)
+	{
+		for (j = k; j < _strlen(av[stockAc]); j++)
+		{
+			*(s + k) = av[i];
+			k++;
+		}
+	}
+
+	return (s);
+}
