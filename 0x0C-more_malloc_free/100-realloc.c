6fc5c7159eda662ab232071d46a055e83dc388cf
fourbet <ofourbet@yahoo.fr>
2020-10-15 13:14:37 +0000

    first try

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * _realloc - reallocates memory for an array, using malloc
+ *
+ * @old_size: unsigned int
+ * @new_size: unsigned int
+ * @ptr: pointer
+ * Return: pointer to the allocated memory
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *r;
+
+	if (ptr == NULL)
+	{
+		r = malloc(new_size);
+		if (r == NULL)
+			return (NULL);
+		return (r);
+	}
+	if (new_size == old_size)
+		return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	r = malloc(new_size);
+	if (r == NULL)
+		return (NULL);
+	return (r);
+
+}
