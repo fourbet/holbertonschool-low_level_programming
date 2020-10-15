6a2c3bfcc1011086028d7f057936bfe77bb6f557
fourbet <ofourbet@yahoo.fr>
2020-10-15 12:36:04 +0000

    fixed

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,16 +11,16 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	char *i;
+	void *r;
 	unsigned int j;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	i = malloc(size * nmemb);
-	if (i == NULL)
+	r = malloc(size * nmemb);
+	if (r == NULL)
 		return (NULL);
 	for (j = 0; j < nmemb; j++)
-		i[j] = 0;
-	return (i);
+		((char *)r)[j] = 0;
+	return (r);
 
 }
