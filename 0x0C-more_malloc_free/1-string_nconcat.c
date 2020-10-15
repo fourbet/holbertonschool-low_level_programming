6fcef312813c091caf4d164d3f7dd1034dd3cb87
fourbet <ofourbet@yahoo.fr>
2020-10-15 08:20:29 +0000

    first commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,56 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * _strlen - length of string
+ *
+ *@str: pointer to a string
+ *
+ * Return: int, length
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+/**
+ * string_nconcat - concatenates two strings
+ *
+ * @s1: pointer to a char
+ * @s2: pointer to a char
+ * @n: unsigned int
+ * Return: newly allocated space un memory
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *s;
+	unsigned int i, j, k, len1, len2;
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	if (n < len2)
+		len2 = n;
+
+	s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len1; i++)
+		*(s + i) = *(s1 + i);
+	for (j = i; j < i + len2; j++)
+	{
+		*(s + j) = *(s2 + k);
+		k++;
+	}
+	*(s + j) = '\0';
+	return (s);
+}
