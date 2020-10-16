a4dd21884490bea66eb964a03e83f6ed6baf817c
fourbet <ofourbet@yahoo.fr>
2020-10-16 08:16:08 +0000

    fixed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -43,7 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < len2)
 		len2 = n;
 	s = malloc(sizeof(char) * (len1 + len2 + 1));
-	printf(" len1 + len2 + 1 %d\n",len1 + len2 + 1);
 	if (s == NULL)
 		return (NULL);
 	for (i = 0; i < len1; i++)
