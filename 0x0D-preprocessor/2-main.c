939adbe7062f477248b0478b3dff863ce8c342b6
fourbet <ofourbet@yahoo.fr>
2020-10-19 08:25:13 +0000

    add exercise 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 /**
  * main - prints the name of the file it was compiled from
  *
- * Return : 0
+ * Return: 0
  */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
 	return (0);
 }
