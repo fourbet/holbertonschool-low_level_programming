18d0fe4816ff82cee56d03c9c243721de85af7a4
fourbet <ofourbet@yahoo.fr>
2020-10-19 17:22:23 +0000

    add exercise 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -15,8 +15,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
 	return (d);
 }
