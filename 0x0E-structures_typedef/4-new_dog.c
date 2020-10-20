d133115d0b0fbe6868a2414a5d241000a36cde16
fourbet <ofourbet@yahoo.fr>
2020-10-20 07:53:59 +0000

    add exercise 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * _strlen - length of a string
+ *
+ * @str: pointer to a char
+ *
+ * Return: int
+ */
+int _strlen(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+/**
  * new_dog - creates a new dog
  *
  * @name: pointer to a char
@@ -12,11 +27,34 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *d;
+	char *s;
+	char *o;
+	int i = 0;
+	int j = 0;
+
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d.name = name;
-	d.age = age;
-	d.owner = owner;
+	s = malloc(sizeof(char) * _strlen(name) + 1);
+	if (s == NULL)
+		return (NULL);
+	o = malloc(sizeof(char) * _strlen(owner) + 1);
+	if (o == NULL)
+		return (NULL);
+	d->age = age;
+	while (name[i] != '\0')
+	{
+		s[i] = name[i];
+		i++;
+	}
+	s[i] = '\0';
+	d->name = s;
+	while (owner[j] != '\0')
+	{
+		o[j] = owner[j];
+		j++;
+	}
+	o[j] = '\0';
+	d->owner = 0;
 	return (d);
 }
