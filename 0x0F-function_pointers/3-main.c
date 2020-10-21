fb90c6f88b2d6c0c5b3ce21df94f9b9042833947
fourbet <ofourbet@yahoo.fr>
2020-10-21 19:19:51 +0000

    add exercise 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main program
+ * @ac: int, nbr parameters
+ * @av: pointer to an array of char
+ *
+ * Return: int
+ */
+int main(int ac, char *av[])
+{
+	int res = 0;
+	int num1 = 0;
+	int num2 = 0;
+
+	if (ac != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(*(av + 1));
+	num2 = atoi(*(av + 3));
+	if (av[2][0] == '/' && num2 == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (av[2][0] == '+' || av[2][0] == '-' || av[2][0] == '*'
+	     || av[2][0] == '/' || av[2][0] == '%')
+	{
+		res = get_op_func(av[2])(num1, num2);
+		printf("%d\n", res);
+		return (0);
+	}
+	printf("Error\n");
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - returns the sum
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the result of the division
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division
+ *
+ * @a: int
+ * @b: int
+ *
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
