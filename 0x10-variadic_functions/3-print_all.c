bf703b63adf4444aa8b97bc0929c155afa2b4493
fourbet <ofourbet@yahoo.fr>
2020-10-23 15:46:48 +0000

    add exercise 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,49 @@
 #include "variadic_functions.h"
 /**
+ * print_string - prints string
+ *
+ * @s: pointer to char
+ *
+ * Return: void
+ */
+void print_string(va_list valist)
+{
+	printf("%s", va_arg(valist, char *));
+}
+/**
+ * print_char - prints char
+ *
+ * @c: char
+ *
+ * Return: void
+ */
+void print_char(va_list valist)
+{
+	printf("%c", va_arg(valist, int));
+}
+/**
+ * print_float - prints float
+ *
+ * @f: float
+ *
+ * Return: void
+ */
+void print_float(va_list valist)
+{
+	printf("%f", va_arg(valist, double));
+}
+/**
+ * print_int - prints int
+ *
+ * @i: int
+ *
+ * Return: void
+ */
+void print_int(va_list valist)
+{
+	printf("%d", va_arg(valist, int));
+}
+/**
  * print_all - prints anything
  *
  * @format: const pointer of const char
@@ -8,41 +52,39 @@
  */
 void print_all(const char *const format, ...)
 {
-	va_list valist;	
+	va_list valist;
 	int i = 0;
+	int j = 0;
+	char *separator = ", ";
 	int ignored = 0;
-	char *s;
-	char *separator =", ";
+
+	format_t print[] = {
+		{'c', print_char},
+		{'f', print_float},
+		{'i', print_int},
+		{'s', print_string},
+		{'\0', NULL}
+	};
 
 	va_start(valist, format);
-	while (format[i] != '\0')
+
+	while (format[i])
 	{
-		switch (format[i])
+		while (print[j].type != '\0')
 		{
-		case 's' :
-			s = va_arg(valist, char *);
-			if (s)
-				printf("%s", s);
+			if (format[i] == print[j].type)
+			{
+				print[j].f(valist);
+			}
 			else
-				printf("(nil)");
-			break;
-		case 'i' :
-			printf("%i", va_arg(valist, int));
-			break;
-		case 'f' :
-			printf("%f", va_arg(valist, double));
-			break;
-		case 'c' :
-			printf("%c", (char) va_arg(valist, int));
-			break;
-		default :
-			ignored = 1;
-			break;
+				ignored++;
+			j++;
 		}
-		if (format[i + 1] != '\0' && !ignored)
-                        printf("%s", separator);
-		i++;
+		if (format[i + 1] != '\0' && ignored != 4)
+			printf("%s", separator);
 		ignored = 0;
+		j = 0;
+		i++;
 	}
 	va_end(valist);
 	printf("\n");
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,19 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+/**
+ * struct format_s - Struct s
+ *
+ * @type: char
+ * @f: the function associated
+ */
+typedef struct format
+{
+	char type;
+	void (*f)(va_list a);
+} format_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
