944c74b32d6543103d3908f75af309c16607aa2d
fourbet <ofourbet@yahoo.fr>
2020-10-24 11:42:05 +0000

    fix separator == NULL

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,13 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
-		if (separator == NULL)
-		{
+		if (separator != NULL)
 			if (i != n - 1)
-				printf(" ");
-		}
-		else if (i != n - 1)
-			printf("%s", separator);
+				printf("%s", separator);
 	}
 	va_end(valist);
 	printf("\n");
