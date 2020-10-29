cb48d2d5f949e9f5c041be9d28dcf3e8e9d43bd9
fourbet <ofourbet@yahoo.fr>
2020-10-29 09:52:35 +0000

    simplify the function print_list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,7 +13,7 @@ size_t print_list(const list_t *h)
 {
 	size_t counter = 0;
 
-	while (h->next != NULL)
+	while (h != NULL)
 	{
 		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
@@ -22,10 +22,5 @@ size_t print_list(const list_t *h)
 		h = h->next;
 		counter++;
 	}
-	if (h->str != NULL)
-		printf("[%d] %s\n", h->len, h->str);
-	else
-		printf("[0] (nil)\n");
-	counter++;
 	return(counter);
 }
