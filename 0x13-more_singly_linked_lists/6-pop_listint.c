46496a72d5059c3fe3978f33ff4e7e9dcdc7f927
fourbet <ofourbet@yahoo.fr>
2020-11-02 16:51:06 +0000

    feat (pop_listint)

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * pop_listint - deletes the head node of a listint_t linked list
+ *
+ * @head: pointer to a pointer to a listint_t
+ *
+ * Return: returns the head nodes data (n)
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *current;
+	int res = 0;
+
+	current = *head;
+	res = current->n;
+	*head = current->next;
+	free(current);
+
+	return (res);
+}
