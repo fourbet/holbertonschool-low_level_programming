9e4e2f16057b3d195605f84d78f0a893d1972860
fourbet <ofourbet@yahoo.fr>
2020-11-03 18:02:51 +0000

    add case loop from the beginning

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -23,25 +23,28 @@ size_t print_listint_safe(const listint_t *head)
 	{
 		if (loop == 2 && (p == q))
 		{
-			printf("-> [%p] %d\n\n",(void *) p, p->n);
+			printf("-> [%p] %d\n\n", (void *) p, p->n);
 			return (count);
 		}
 		if ((p == q) && count != 0)
 		{
 			loop = 1;
 			p = begin;
+			if (p == q)
+			{
+				printf("-> [%p] %d\n\n", (void *) p, p->n);
+				return (count);
+			}
 			loop = 2;
 		}
 			count++;
-			printf("[%p] %d\n",(void *) q, q->n);
+			printf("[%p] %d\n", (void *) q, q->n);
 			q = q->next;
 			if (p)
 			{
 				p = p->next;
 				if (p && loop == 0)
-				{
 					p = p->next;
-				}
 			}
 			if (!p && !q)
 				return (count);
