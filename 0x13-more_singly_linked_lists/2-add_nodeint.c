588f6c88ea54c8cd5d1e4596989e8d71c0fd90d6
fourbet <ofourbet@yahoo.fr>
2020-11-03 08:29:00 +0000

    delete spaces

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -4,12 +4,10 @@
  * add_nodeint - adds a new node at the beginning of a listint_t list
  *
  * @head: pointer to a pointer to a listint_h
- *
  * @n: const int
  *
  * Return: size_t
  */
-
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
