59141f910cc7bcffdda37fedf27a11e9685df776
fourbet <ofourbet@yahoo.fr>
2020-11-04 09:58:26 +0000

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,16 +18,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int count = 0, passed = 0;
 
 	current = *head;
-	if (current == NULL && idx == 0)
-	{
-		new = malloc(sizeof(listint_t));
-		if (new == NULL)
-			return (NULL);
-		new->n = n;
-		*head = new;
-		count++;
-		passed = 1;
-	}
 	while (current)
 	{
 		if (count == idx)
