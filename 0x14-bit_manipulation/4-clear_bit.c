eadc63acab1a43e31ec7bac686c3b8bf60a3e91d
fourbet <ofourbet@yahoo.fr>
2020-11-05 14:38:04 +0000

    feat (clear_bit)

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ *
+ * @n: pointer to the number
+ * @index: index
+ *
+ * Return: 1 (Sucess), -1 (Fails)
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	*n = *n & ~(1 << index);
+	return (1);
+}
