1c4736d2ae908261641a0da2fcd9961f3145c562
fourbet <ofourbet@yahoo.fr>
2020-11-05 08:49:46 +0000

    feat (README, header file && 0-binary_to_uint)

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ *
+ * @b: pointer to a string of 0 and 1 chars
+ *
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int res = 0;
+	unsigned int i = 0;
+
+	if (b == NULL)
+		return (0);
+
+	while (b[i] != '\0')
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+		res <<= 1;
+		res += b[i] - '0';
+		i++;
+	}
+
+	return (res);
+}
