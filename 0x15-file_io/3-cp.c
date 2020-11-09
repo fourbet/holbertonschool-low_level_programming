fd30f0f0f4edb6d5541c46e12af6859a01fe3d10
fourbet <ofourbet@yahoo.fr>
2020-11-09 18:25:08 +0000

    feat (cp)

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen - return length of string
+ *
+ * @s: string
+ *
+ * Return: int
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+/**
+ * main - copies the content of a file to another file
+ *
+ * @ac: number of arguments
+ * @av: pointer to an array of char
+ *
+ *Return: 0 (Success)
+ */
+int main(int ac, char **av)
+{
+	int fd_write;
+	int fd_read;
+	ssize_t bytesR = 0, bytesW = 0;
+	char *buf;
+	int fd_close;
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd_read = open(av[1], O_RDONLY);
+	if (fd_read == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	fd_write = open(av[2], O_WRONLY | O_TRUNC);
+	if (fd_write == -1)
+		fd_write = open(av[2], O_WRONLY | O_CREAT, 0664);
+	if (fd_write == -1)
+	{
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		exit(99);
+	}
+	buf = malloc(sizeof(char) * 1024);
+	if (buf == NULL)
+		return (0);
+	bytesR = read(fd_read, buf, 1024);
+	bytesW = write(fd_write, buf, bytesR);
+	if (bytesR == -1)
+		return (-1);
+	if (bytesW == -1)
+		return (-1);
+	fd_close = close(fd_read);
+	if (fd_close == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_read);
+		exit(100);
+	}
+	fd_close = close(fd_write);
+	if (fd_close == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_write);
+		exit(100);
+	}
+	return (0);
+}
