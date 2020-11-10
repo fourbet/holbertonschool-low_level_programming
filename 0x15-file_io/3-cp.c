3d40abbe2f1d1b67678f2bc159d630a13fed2ce4
fourbet <ofourbet@yahoo.fr>
2020-11-10 08:39:16 +0000

    check write return value

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int ac, char **av)
 	if (fd_write == -1)
 		fd_write = open(av[2], O_WRONLY | O_CREAT, 0664);
 	if (fd_write == -1)
-	       dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	while ((bytesR = read(fd_read, buf, 1024)) > 0)
 		bytesW = write(fd_write, buf, bytesR);
 	if (bytesR == -1)
