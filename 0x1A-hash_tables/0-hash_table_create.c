f3582a87785aadd1fd7f803eb353650815bcc7af
fourbet <ofourbet@yahoo.fr>
2021-01-14 11:32:15 +0000

    add creation of a hash table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the newly creates hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_tab;
+
+	hash_tab = malloc(sizeof(*hash_node_t) * size);
+	if (hash_tab == NULL)
+		return (NULL);
+	return (hash_tab);
+}
