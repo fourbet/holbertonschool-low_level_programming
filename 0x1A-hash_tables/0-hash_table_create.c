0a5eb8a9424138e7829eed0f85f885c43a3f86e3
fourbet <ofourbet@yahoo.fr>
2021-01-14 13:42:32 +0000

    correct (foo*) to foo *

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,7 +10,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_tab;
 
-	hash_tab = malloc(sizeof(*hash_node_t) * size);
+	hash_tab = malloc(sizeof(hash_node_t *) * size);
 	if (hash_tab == NULL)
 		return (NULL);
 	return (hash_tab);
