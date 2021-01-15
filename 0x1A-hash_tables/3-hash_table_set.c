7c9da91e329cb52568ba1f068b4314464ce56e7a
fourbet <ofourbet@yahoo.fr>
2021-01-15 13:16:20 +0000

    add hash table set

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,47 @@
+#include "hash_tables.h"
+/**
+ * add_node - adds a new node at the beginning of a hash_node_t
+ *
+ * @head: pointer to a pointer to a hash_node_t
+ * @key: key
+ * @value: value associated to the key
+ *
+ * Return: the adress of the new element or NULL if failed
+ */
+hash_node_t *add_node(hash_node_t **head, const char *key, const char *value)
+{
+	hash_node_t *new_node = NULL;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: hash table
+ * @key: key
+ * @value: value associated with the key
+ *
+ * Return: 1 (SUCCESS), 0 (OTHERWISE)
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	unsigned long int size;
+	hash_node_t **array;
+
+	if (ht == NULL || strcmp(key, "") == 0)
+		return (0);
+	size = ht->size;
+	printf("size : %lu\n", size);
+	index = key_index((unsigned char *)key, size);
+	printf("index : %lu\n", index);
+	array = ht->array;
+	array[index] = add_node(&array[index], key, value);
+	return (1);
+}
