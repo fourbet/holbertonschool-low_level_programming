8098872834c42359481552fd20b6ba57ca745ed8
fourbet <ofourbet@yahoo.fr>
2021-01-15 11:22:12 +0000

    correct size to malloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,11 +8,14 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_tab;
+	hash_table_t *hash_tab = NULL;
 
-	hash_tab = malloc(sizeof(hash_node_t *) * size);
+	hash_tab = malloc(sizeof(hash_table_t));
 	if (hash_tab == NULL)
 		return (NULL);
 	hash_tab->size = size;
+	hash_tab->array = malloc(sizeof(hash_node_t*) * size);
+	if (hash_tab->array == NULL)
+		return (NULL);
 	return (hash_tab);
 }
