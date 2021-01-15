acc54227802f9370651be76b2a180cd69ca790af
fourbet <ofourbet@yahoo.fr>
2021-01-15 14:36:00 +0000

    add hash table delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: pointer to a hash table
+ *
+ * Return: void
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t **array;
+	unsigned long int count;
+	hash_node_t *current;
+	hash_node_t *tmp;
+
+	if (ht)
+	{
+		array = ht->array;
+		while (count < ht->size)
+		{
+			current = array[count];
+			while (current != NULL)
+			{
+				tmp = current;
+				current = current->next;
+				free(tmp);
+			}
+			count++;
+		}
+		free(ht);
+	}
+}
