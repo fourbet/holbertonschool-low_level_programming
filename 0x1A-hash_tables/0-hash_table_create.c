3e1066824a59a0dd163f157cf578bca5f689eb8e
fourbet <ofourbet@yahoo.fr>
2021-01-15 11:23:03 +0000

    betty compliant

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -14,7 +14,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_tab == NULL)
 		return (NULL);
 	hash_tab->size = size;
-	hash_tab->array = malloc(sizeof(hash_node_t*) * size);
+	hash_tab->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_tab->array == NULL)
 		return (NULL);
 	return (hash_tab);
