e2a15f158889e707996778ebbefb82649205bacf
fourbet <ofourbet@yahoo.fr>
2021-01-17 17:52:53 +0000

    add initialization of the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_tab = NULL;
+	unsigned long int i = 0;
 
 	hash_tab = malloc(sizeof(hash_table_t));
 	if (hash_tab == NULL)
@@ -17,5 +18,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_tab->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_tab->array == NULL)
 		return (NULL);
+	for (i = 0; i < size; i++)
+	{
+		hash_tab->array[i] = NULL;
+	}
 	return (hash_tab);
 }
