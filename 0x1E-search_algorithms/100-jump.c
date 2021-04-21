163c6617e5995aa54e5c5e7842afdf52352ba151
fourbet <ofourbet@yahoo.fr>
2021-04-21 14:58:08 +0000

    fix()

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -1,19 +1,6 @@
 #include "search_algos.h"
 #include <math.h>
 /**
- * min_nbr - find the minimal value between two numbers
- * @a: size_t
- * @b: size_t
- * Return: size_t
- */
-size_t min_nbr(size_t a, size_t b)
-{
-	if (a <= b)
-		return (a);
-	else
-		return (b);
-}
-/**
  * jump_search - searches for a value in an array of integers
  * using the Jump search algorithm
  * @array: pointer to the first element of the array to search in
@@ -37,9 +24,8 @@ int jump_search(int *array, size_t size, int value)
 		if (b >= size)
 			break;
 	}
-	b = min_nbr(b, size - 1);
 	printf("Value found between indexes [%d] and [%lu]\n", a, b);
-	while (array[a] <= (int)b)
+	while (a <= (int)b)
 	{
 		printf("Value checked array[%d] = [%d]\n", a, array[a]);
 		if (array[a] == value)
