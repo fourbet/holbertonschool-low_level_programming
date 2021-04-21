26fba88349079c3f04f0d5139713199b007721a2
fourbet <ofourbet@yahoo.fr>
2021-04-21 14:04:25 +0000

    feat(): interpolation algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -30,9 +30,9 @@ int interpolation_search(int *array, size_t size, int value)
 		if (array[pos] == value)
 			return (pos);
 		else if (array[pos] < value)
-			low++;
+			low = pos + 1;
 		else if (array[pos] > value)
-			high--;
+			high= pos - 1;
 	}
 	return (-1);
 }
